1ec456e6c039fec513ee61abaa4f639c6c6125df
MohammadYehya <mohammad7446@gmail.com>
2024-12-31 22:25:57 +0500

    completed 3

--- _03_Shadowing/src/main.rs
+++ _03_Shadowing/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let x: u32 = 10;
+    let x: f32 = 11.11;    //x is being overwritten
+
+    println!("{x}");
+
+
+    //This applies to when scope changes as well
+
+    let x: u32 = 12;
+
+    {
+        let x = x+2;
+        println!("{x}");    //prints 14
+    }
+    println!("{x}");        //prints 12
 }
