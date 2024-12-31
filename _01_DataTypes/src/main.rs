43970c3992bf3c93551e7d74d53b65ff9fe93311
MohammadYehya <mohammad7446@gmail.com>
2024-12-31 19:01:16 +0500

    Initial Commit

---
+++ _01_DataTypes/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    //Unsigned integer data types = u8, u16, u32, u64
+    let x1: u8 = 128;
+
+    //Signed integer data types = i8, i16, i32, i64
+    let x2: i8 = -127;  
+    
+    //Floating point numbers data types = f32, f64
+    let x3: f32 = 1.234;
+    
+    //Character data type = char
+    let x4: char = 'a';
+
+    //Boolean data types = bool
+    let x5: bool = true;
+
+    println!("{x1}, {x2}, {x3}, {x4}, {x5}");
+}
