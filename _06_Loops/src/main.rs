e46325a6e46e6a83f6a8fc1a37a4bef093cfec0d
MohammadYehya <mohammad7446@gmail.com>
2025-01-26 23:25:54 +0500

    Update main.rs

--- _06_Loops/src/main.rs
+++ _06_Loops/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // While loops
+    let mut num: i8 = 3;
+
+    while num != 0 {
+        println!("{num}!");
+        num -= 1;
+    }
+
+    // For loops
+    let a: [i8; 5] = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    // Unconditional Loops
+    num = 5;
+    loop {
+        println!("again!");
+        if num == 0 {break;}
+        num -= 1;
+    }
 }
