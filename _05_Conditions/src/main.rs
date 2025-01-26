48e1b0e778450da7d04e6a83632bb55d9f688527
MohammadYehya <mohammad7446@gmail.com>
2025-01-26 23:18:27 +0500

    Update main.rs

--- _05_Conditions/src/main.rs
+++ _05_Conditions/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let num: i8 = 10;
+
+    // Classical if operator
+    if num == 10 {
+        println!("Number is 10!");
+    }
+    else if num == 5 {
+        println!("Number is 5!");
+    }
+    else {
+        println!("Number is neither 10 nor 5!");
+    }
+
+    // if used as a ternary operator
+    let num:i8 = if true {5} else {6};
+    println!("{num}");
 }
