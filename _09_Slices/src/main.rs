2142055dd9d86ffe1643ec93b5099d1bc1c74af0
MohammadYehya <mohammad7446@gmail.com>
2025-03-14 13:45:43 +0500

    Update main.rs

--- _09_Slices/src/main.rs
+++ _09_Slices/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    // A slice is a reference to any collection of data
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+    
+    // s.clear();       This requires a mutable reference and there exists an immutable reference that is being used later
+
+    println!("{word}");
+
+    s.clear();          // Now this works!
+
+    // Similarly here is a slice of an i8 array
+
+    let a: [i8; 5] = [1,2,3,4,5];
+    let r1 = &a[..1];
+    println!("{}", r1[0]);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
