0e42f5677d15032f73e61d4bf40eca4786d6771e
MohammadYehya <mohammad7446@gmail.com>
2025-03-14 13:45:36 +0500

    Update main.rs

--- _06_Loops/src/main.rs
+++ _06_Loops/src/main.rs
@@ -21,4 +21,31 @@ fn main() {
         if num == 0 {break;}
         num -= 1;
     }
+
+    // Return value with break
+    num = loop {
+        break 20;
+    };
+    println!("Loop returned with value: {num}");
+
+    // Loop Labels
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
