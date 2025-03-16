5a77d411282f4f31e304e274156c7f60e0163465
MohammadYehya <mohammad7446@gmail.com>
2025-03-16 12:42:46 +0500

    Update main.rs

--- _13_CommonCollections/src/main.rs
+++ _13_CommonCollections/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     {
         let _v1: Vec<i8> = Vec::new();
         // or
-        let _v1 = vec![1i8,2,3];
+        let _v1 = vec![1i8, 2, 3];
 
         let mut v = Vec::new();
         v.push(5i8);
@@ -16,13 +16,12 @@ fn main() {
         // or
         if let Some(x) = v.get(2) {
             println!("{x}");
-        }
-        else {
+        } else {
             println!("Empty");
         }
 
         // Since we have an immutable reference, if we were to modify the vector between creating and using the reference, that would be an error.
-        // This is because vectors put the values next to each other in memory, adding a new element onto the end of the vector might require allocating new memory and copying the old elements to the new space 
+        // This is because vectors put the values next to each other in memory, adding a new element onto the end of the vector might require allocating new memory and copying the old elements to the new space
 
         for i in &v {
             println!("{i}");
@@ -39,25 +38,54 @@ fn main() {
             Float(f64),
             Text(String),
         }
-    
-        let row = vec![
+
+        let row: Vec<SpreadsheetCell> = vec![
             SpreadsheetCell::Int(3),
             SpreadsheetCell::Text(String::from("blue")),
             SpreadsheetCell::Float(10.12),
         ];
-        
+
         for i in &row {
             println!("{i:?}");
         }
     }
 
-    // Strings
-    {
-
-    }
-
     // Hash Maps
     {
+        use std::collections::HashMap;
 
+        let mut scores: HashMap<String, i32> = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+
+        println!("{:?}", scores.get("Blue1").copied().unwrap_or(0));
+        // Since the get method returns a Option<&T>, we will call the copied function to convert it to Option<T>.
+        // Then we use the unwrap_or function to unwrap the Option type and get the actual values.
+
+        for (key, value) in &scores {
+            println!("{key}: {value}");
+        }
+
+        // A HashMap takes ownership of types that dont implement the copy trait
+
+        // or_insert is defined to return a mutable reference to the value of a key if that key exists, and if not, it inserts the parameter as the new value for this key
+        {
+            scores.entry(String::from("Yellow")).or_insert(100);
+            scores.entry(String::from("Green")).or_insert(50);
+            println!("{:?}", scores);
+        }
+
+        // Since or_insert returns a mutable reference, we can use it to update a value in a Map
+        {
+            let text = "hello world wonderful world";
+            let mut map = HashMap::new();
+
+            for word in text.split_whitespace() {
+                let count = map.entry(word).or_insert(0);
+                *count += 1;
+            }
+
+            println!("{map:?}");
+        }
     }
 }
