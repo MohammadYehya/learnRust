e23e86a19305ee62509755e50388b36081132ff2
MohammadYehya <mohammad7446@gmail.com>
2025-03-18 22:00:21 +0500

    Update main.rs

--- _15_GenericTypes/src/main.rs
+++ _15_GenericTypes/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    // The purpose of generic types is to reduce redundant code
+    // It is simply templates in C++
+
+    {
+        fn printarr<T: std::fmt::Display>(arr: &[T]) {
+            for i in arr {
+                print!("{i}");
+            }
+        }
+        let arr1 = [1, 2, 3, 4];
+        let arr2 = ['a', 'b', 'c', 'd'];
+        printarr(&arr1);
+        printarr(&arr2);
+    }
+
+    {
+        struct Point<T> {
+            _x: T,
+            _y: T,
+        }
+
+        impl<T> Point<T> {
+            fn _x(&self) -> &T {
+                &self._x
+            }
+        }
+
+        let _integer = Point { _x: 5, _y: 10 };
+        let _float = Point { _x: 1.0, _y: 4.0 };
+    }
 }
