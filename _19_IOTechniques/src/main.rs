2d68b5bcb0548bf694de57cfb1db596148a96ae6
MohammadYehya <mohammad7446@gmail.com>
2025-03-21 18:29:49 +0500

    Update main.rs

--- _19_IOTechniques/src/main.rs
+++ _19_IOTechniques/src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    {
+        // Command line arguments
+
+        // cargo run -- abc example.txt
+        let args: Vec<String> = env::args().collect();
+        println!("{}", args[2]);
+    }
+
+    {
+        // Reading a txt file from root level of project
+
+        let contents = fs::read_to_string("txt.txt").expect("Should have been able to read the file");
+        println!("{contents}");
+    }
 }
