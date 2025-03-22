0659c62d6f0ec73d4f50ab9c41c47cefe0ac11bc
MohammadYehya <mohammad7446@gmail.com>
2025-03-22 17:43:08 +0500

    Update main.rs

--- _20_Closures/src/main.rs
+++ _20_Closures/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // Closures are basically arrow functions in other languages
+
+    fn _add_one_v1(x: u32) -> u32 {
+        x + 1
+    }
+    let _add_one_v2 = |x: u32| -> u32 { x + 1 };
+    let _add_one_v3 = |x :u32| {x + 1};
+    let _add_one_v4 = |x: u32| x + 1;
+    // All of the above are the same
+
 }
